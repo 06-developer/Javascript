b487d04d6cb98183b1ba2e7eea61715c7923b31b
06-developer <bahromrustamov4244@gmail.com>
2023-11-03 11:04:03 +0300

    Uy ishi

---
+++ 1/index.js
@@ -0,0 +1,19 @@
+//1. String //
+let string = 'Hello world'
+console.log(String)
+//2. Number //
+const num = 15
+console.log(num)
+//3. Boolean //
+const isNum = true
+console.log(isNum)
+//4. Null  //
+const num3 = null
+//5. Undefined //
+const num4 = undefined
+
+console.log (typeof string)
+console.log (typeof num)
+console.log (typeof isNum)
+console.log (typeof num3)
+console.log (typeof num4)
---
+++ 2/index.js
@@ -0,0 +1,37 @@
+// let //
+// let yosh = 23;
+// console.log(yosh)
+// const //
+// const id = 'AA0';
+// id= 'AA1'
+// var //
+// var yosh = 23;
+// yosh =24;
+// let boolean =true
+// let matn = String(boolean);
+// console.log (typeof boolean);
+// let satr = `${boolean}`;
+// console.log(typeof satr)
+// let number = 2;
+// console.log(number);
+// let satr = String(number);
+// console.log(satr)
+// let string = "5"
+// let string2 = "10"
+// console.log(string + string2);
+// console.log(Number(+string) + Number(+string2))
+// let symbol = "*"
+// console.log(Number(symbol))
+// alert("5" + "10")
+// alert("5" * "10")
+// alert("5" - "10")
+// alert("5" / "10")
+// let n = null
+// console.log(+n)
+// let t = false
+// console.log(+false)
+// console.log (Boolean())
+// console.log ("s\na")
+// console.log ("s\ta")
+
+
